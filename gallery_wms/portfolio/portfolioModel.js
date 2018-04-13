8b05bf8293725d028a2fbd965805f8a6ead212c6
Jeff Sprague <jeffrsprague@gmail.com>
2018-04-12 20:37:31 -0700

    Removing some images

--- gallery_wms/portfolio/portfolioModel.js
+++ gallery_wms/portfolio/portfolioModel.js
@@ -18,46 +18,21 @@ var PORTFOLIO_MODEL = {};
     // ********************************************
     this.getPortfolioData = function () {
         var portfolio = [];
-        portfolio[0] = {};
-        portfolio[0].label = 'Art by Wendy';
-        portfolio[0].images = [
-            { url: 'images/artwork-web/1.jpg' },
-            { url: 'images/artwork-web/2.jpg' },
-            { url: 'images/artwork-web/3.jpg' },
-            { url: 'images/artwork-web/4.jpg' },
-            { url: 'images/artwork-web/8.jpg' },
-            { url: 'images/artwork-web/11.jpg' },
-            { url: 'images/artwork-web/12.jpg' },
-            { url: 'images/artwork-web/13.jpg' },
-            { url: 'images/artwork-web/14.jpg' },
-            { url: 'images/artwork-web/15.jpg' },
-            { url: 'images/artwork-web/16.jpg' },
-            { url: 'images/artwork-web/18.jpg' },
-            { url: 'images/artwork-web/19.jpg' },
-            { url: 'images/artwork-web/21.jpg' },
-            { url: 'images/artwork-web/22.jpg' },
-            { url: 'images/artwork-web/23.jpg' }
-        ];
 
-        portfolio[1] = {};
-        portfolio[1].label = 'Pet Portraits';
-        portfolio[1].images = [
+        portfolio[0] = {};
+        portfolio[0].label = 'Pet Portraits';
+        portfolio[0].images = [
             { url: 'images/pets-web/1.jpg' },
             { url: 'images/pets-web/2.jpg' },
             { url: 'images/pets-web/3.jpg' },
             { url: 'images/pets-web/4.jpg' },
             { url: 'images/pets-web/5.jpg' },
             { url: 'images/pets-web/7.jpg' },
-            { url: 'images/pets-web/8.jpg' },
-            { url: 'images/pets-web/9.jpg' },
-            { url: 'images/pets-web/10.jpg' },
             { url: 'images/pets-web/11.jpg' },
             { url: 'images/pets-web/12.jpg' },
             { url: 'images/pets-web/13.jpg' },
             { url: 'images/pets-web/14.jpg' },
             { url: 'images/pets-web/15.jpg' },
-            { url: 'images/pets-web/16.jpg' },
-            { url: 'images/pets-web/17.jpg' },
             { url: 'images/pets-web/18.jpg' },
             { url: 'images/pets-web/19.jpg' },
             { url: 'images/pets-web/20.jpg' },
@@ -70,6 +45,30 @@ var PORTFOLIO_MODEL = {};
             { url: 'images/pets-web/27.jpg' },
             { url: 'images/pets-web/28.jpg' },
             { url: 'images/pets-web/29.jpg' }
+        ];
+
+        portfolio[1] = {};
+        portfolio[1].label = 'Art by Wendy';
+        portfolio[1].images = [
+            { url: 'images/artwork-web/1.jpg' },
+            { url: 'images/artwork-web/2.jpg' },
+            { url: 'images/artwork-web/3.jpg' },
+            { url: 'images/artwork-web/4.jpg' },
+            { url: 'images/artwork-web/8.jpg' },
+            { url: 'images/artwork-web/11.jpg' },
+            { url: 'images/artwork-web/12.jpg' },
+            { url: 'images/artwork-web/19.jpg' },
+            { url: 'images/artwork-web/22.jpg' },
+            { url: 'images/artwork-web/23.jpg' }
+        ];
+
+        portfolio[2] = {};
+        portfolio[2].label = 'Miscellaneous';
+        portfolio[2].images = [
+            { url: 'images/pets-web/8.jpg' },
+            { url: 'images/pets-web/16.jpg' },
+            { url: 'images/pets-web/17.jpg' },
+            { url: 'images/artwork-web/21.jpg' },
         ];
 
         return portfolio;
